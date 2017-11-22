ddee5460ad605f1870ed17f73f8d1690aa94638b
ChrisVieira <sarge3do99@gmail.com>
2017-11-21 20:35:17 -0500

    Basic Functions Finished
    Drives using joystick from 0% to 100% starting when joystick.getY() is
    at least 10% in one direction.

---
+++ src/org/usfirst/frc5112/Robot2015UpdatedProgram/commands/OperatorControl.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc5112.Robot2015UpdatedProgram.commands;
+
+import org.usfirst.frc5112.Robot2015UpdatedProgram.OI;
+import org.usfirst.frc5112.Robot2015UpdatedProgram.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class OperatorControl extends Command {
+
+    public OperatorControl() {
+    	requires(Robot.drivetrain);
+    }
+
+    protected void initialize() {
+    	Robot.drivetrain.stop();
+    }
+
+    protected void execute() {
+    	Robot.drivetrain.operatorControl(OI.joystick);
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    	Robot.drivetrain.stop();
+    }
+
+    protected void interrupted() {
+    	Robot.drivetrain.stop();
+    }
+}
